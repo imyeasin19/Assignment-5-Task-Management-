9e2d1da42d6344c7d94363dafd136ae9c7b3b620
imyeasin19 <yeasin.mia@northsouth.edu>
2025-03-03 11:20:49 +0600

    made some changes in code and completed the task and total task function

--- script/completed.js
+++ script/completed.js
@@ -1,10 +1,23 @@
-document.getElementById('completed-btn1').addEventListener('click', function (event){
-    event.preventDefault();
-    alert("Board Update Successfully");
-    const container = document.getElementById('activity-container');
-    const p = document.createElement('p');
-    const name = document.getElementById('box-1').innerText;
-    p.innerText = `You have completed the task ${name}`;
-    container.appendChild(p); 
-    document.getElementById('completed-btn1').disabled = true;
-})
+document.querySelectorAll('.completed-btn').forEach(button => {
+    button.addEventListener('click', function (event) {
+        event.preventDefault();
+        alert("Board Update Successfully");
+        const container = document.getElementById('log-container');
+        const p = document.createElement('p');
+        const name = this.closest('.task-box').querySelector('.task-name').innerText;
+        let currentDate = new Date();
+        const time = currentDate.toLocaleTimeString();
+
+        p.innerText = `You have completed the task "${name}" at ${time}`;
+        container.appendChild(p);
+
+        this.disabled = true;
+
+        const task = getInnerText('task');
+        const sum = task - 1;
+        setInnerText('task', sum);
+        const totaltask = getInnerText('total-task');
+        const total = totaltask + 1;
+        setInnerText('total-task', total);
+    });
+});
